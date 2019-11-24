873611a844ee71cbab0d896272a9dfbd8f578f86
Johny Alexander Mora Méndez <58131021+JohnyDeCoder@users.noreply.github.com>
2019-11-24 01:33:14 -0600

    Condición: For
    Tabla del 1 al 10 usando el ciclo automático "For"...

---
+++ Ciclo FOR - Tabla del 1 al 10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 1; x <= 10; x = x + 1)
+            {
+                Console.WriteLine(" " + x);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Pulse cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
