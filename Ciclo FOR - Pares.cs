7570546c43191c1bf1c2f232f9d0644617b8d7ef
Johny Alexander Mora Méndez <58131021+JohnyDeCoder@users.noreply.github.com>
2019-11-24 01:32:18 -0600

    Condición: For
    Tabla del 2 al 10 contando solamente los números pares usando el ciclo automático "For"...

---
+++ Ciclo FOR - Pares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nethend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 2; x <= 10; x = x + 2)
+            {
+                Console.WriteLine(" " + x);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Pulse cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
