68b7253d5a6d53fcfab434a61ce6ff545d93ee31
Johny Alexander Mora Méndez <58131021+JohnyDeCoder@users.noreply.github.com>
2019-11-24 01:24:40 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, s = 0, o = -1;
+            for (x = 1; x <= 10; x = x + 1)
+            {
+                Console.WriteLine(" " + x);
+                s = s + x + o;
+            }
+           
+            Console.WriteLine("Acumulador: " + s);
+            Console.WriteLine("");
+            Console.WriteLine("Pulse cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
